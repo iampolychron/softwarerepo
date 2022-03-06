d8e564c10efcc75a3a21931fc4bc61d5b36e51d1
iampolychron <70854179+iampolychron@users.noreply.github.com>
2022-03-06 17:25:49 +0200

    Create quickdefrag.bat

---
+++ quickbatch/Windows/quickdefrag.bat
@@ -0,0 +1,9 @@
+@echo off
+echo QuickDefrag - Windows
+title QuickDefrag by iampolychron
+echo By iampolychron
+echo ---------------
+echo This will open the defragment GUI.
+pause
+dfrgui
+title Command Prompt
